211be1d2f3b0dae477242f57412aa6acaf7d7b74
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-07-10 20:35:02 +0530

    few changes

--- dailyQuestion3754.java
+++ dailyQuestion3754.java
@@ -1,6 +1,21 @@
+class dailyQuestion3534 {
+
+    public static void main(String[] args) {
+
+        int n = 1234;
+
+        Solution obj = new Solution();
+
+        long result = obj.sumAndMultiply(n);
+
+        System.out.println("Result = " + result);
+    }
+}
+
 class Solution {
+
     public long sumAndMultiply(int n) {
-        
+
         int i = n;
         int x = 0;
         long f = 0;
@@ -8,27 +23,28 @@ class Solution {
         int[] digits = new int[d];
         int k = 0;
 
-        while(i > 0){
-            if(i % 10 == 0){
-                i = i/10;
-            } 
-            else{
-                digits[k] = i%10;
-                i = i/10;
+        while (i > 0) {
+            if (i % 10 == 0) {
+                i = i / 10;
+            } else {
+                digits[k] = i % 10;
+                i = i / 10;
                 k++;
             }
         }
 
-        for(int j = 0;j<k;j++){
+        for (int j = 0; j < k; j++) {
             x += digits[j];
         }
-        
-        for(int q=k-1;q> -1 ;q++){
-            f = f*10 + digits[q];
-        }
-        long m = x * f;
 
-        if(n == 0) return 0;
+        for (int q = k - 1; q >= 0; q--) {
+            f = f * 10 + digits[q];
+        }
+
+        long m = (long) x * f;
+
+        if (n == 0)
+            return 0;
 
         return m;
     }
