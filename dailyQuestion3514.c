4b40843bdf3040cc4408db1f18a417f4148a531d
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-07-24 15:52:36 +0530

    number of unique XOR triplets - IIin c

---
+++ dailyQuestion3514.c
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        const int MAX = 2048;
+
+        bool dp[4][2048] = {};
+        dp[0][0] = true;
+
+        for (int v : nums) {
+            bool next[4][2048] = {};
+
+            // Don't use current element
+            for (int c = 0; c <= 3; c++) {
+                memcpy(next[c], dp[c], sizeof(dp[c]));
+            }
+
+            // Use current element 1, 2, or 3 times
+            for (int c = 0; c <= 3; c++) {
+                for (int x = 0; x < MAX; x++) {
+                    if (!dp[c][x]) continue;
+
+                    // Take once
+                    if (c + 1 <= 3)
+                        next[c + 1][x ^ v] = true;
+
+                    // Take twice (v ^ v = 0)
+                    if (c + 2 <= 3)
+                        next[c + 2][x] = true;
+
+                    // Take three times
+                    if (c + 3 <= 3)
+                        next[c + 3][x ^ v] = true;
+                }
+            }
+
+            memcpy(dp, next, sizeof(dp));
+        }
+
+        int ans = 0;
+        for (int x = 0; x < MAX; x++) {
+            if (dp[3][x])
+                ans++;
+        }
+
+        return ans;
+    }
+};
