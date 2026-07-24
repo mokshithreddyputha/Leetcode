4ea482e1227caf855c64062e09794f388b0b1aa1
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-07-24 15:53:05 +0530

    few changes

--- dailyQuestion3514.c
+++ dailyQuestion3514.c
@@ -1,47 +1,51 @@
-class Solution {
-public:
-    int uniqueXorTriplets(vector<int>& nums) {
-        const int MAX = 2048;
+#include <stdbool.h>
+#include <string.h>
 
-        bool dp[4][2048] = {};
-        dp[0][0] = true;
+int uniqueXorTriplets(int* nums, int numsSize) {
+    const int MAX = 2048;
 
-        for (int v : nums) {
-            bool next[4][2048] = {};
+    bool dp[4][2048] = {false};
+    bool next[4][2048];
 
-            // Don't use current element
-            for (int c = 0; c <= 3; c++) {
-                memcpy(next[c], dp[c], sizeof(dp[c]));
-            }
+    dp[0][0] = true;
 
-            // Use current element 1, 2, or 3 times
-            for (int c = 0; c <= 3; c++) {
-                for (int x = 0; x < MAX; x++) {
-                    if (!dp[c][x]) continue;
+    for (int i = 0; i < numsSize; i++) {
+        int v = nums[i];
 
-                    // Take once
-                    if (c + 1 <= 3)
-                        next[c + 1][x ^ v] = true;
+        memset(next, 0, sizeof(next));
 
-                    // Take twice (v ^ v = 0)
-                    if (c + 2 <= 3)
-                        next[c + 2][x] = true;
-
-                    // Take three times
-                    if (c + 3 <= 3)
-                        next[c + 3][x ^ v] = true;
-                }
-            }
-
-            memcpy(dp, next, sizeof(dp));
+        // Don't take current element
+        for (int c = 0; c <= 3; c++) {
+            memcpy(next[c], dp[c], sizeof(dp[c]));
         }
 
-        int ans = 0;
-        for (int x = 0; x < MAX; x++) {
-            if (dp[3][x])
-                ans++;
+        // Take current element 1, 2, or 3 times
+        for (int c = 0; c <= 3; c++) {
+            for (int x = 0; x < MAX; x++) {
+                if (!dp[c][x]) continue;
+
+                // Take once
+                if (c + 1 <= 3)
+                    next[c + 1][x ^ v] = true;
+
+                // Take twice (v ^ v = 0)
+                if (c + 2 <= 3)
+                    next[c + 2][x] = true;
+
+                // Take three times
+                if (c + 3 <= 3)
+                    next[c + 3][x ^ v] = true;
+            }
         }
 
-        return ans;
+        memcpy(dp, next, sizeof(dp));
     }
-};
+
+    int ans = 0;
+    for (int x = 0; x < MAX; x++) {
+        if (dp[3][x])
+            ans++;
+    }
+
+    return ans;
+}
