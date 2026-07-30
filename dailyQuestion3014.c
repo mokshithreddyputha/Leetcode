5289fdbbd118c46cf4c44aae0fa64fb6697c090b
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-07-30 23:57:22 +0530

    minimum number of pushes to type word 1 java

---
+++ dailyQuestion3014.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+int minimumPushes(char *word) {
+    int len = strlen(word);
+
+    int x = 0, n = 0, count = 0, k = 0;
+
+    if (len > 8) {
+        n = len / 8;
+        x = len % 8;
+
+        if (n == 1) k = 1;
+        if (n == 2) k = 3;
+        if (n == 3) k = 6;
+
+        count += k * 8 + x * (n + 1);
+    }
+
+    if (len <= 8) {
+        count += len;
+    }
+
+    return count;
+}
