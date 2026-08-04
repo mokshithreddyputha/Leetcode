a36c088428ac5a7407e8f55136770f18b0f37adb
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-08-04 12:05:31 +0530

    Find the Missing Elemens in C++

---
+++ dailyQuestion3731.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingRanges(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for (int i = mn; i <= mx; i++) {
+            if (!st.count(i))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
