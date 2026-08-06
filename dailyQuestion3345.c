396f72dc33d79232ddb6a6d3d9c484b3c40241aa
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-08-06 18:49:17 +0530

    Smallest Divisible Digit Product

---
+++ dailyQuestion3345.c
@@ -0,0 +1,19 @@
+int smallestNumber(int n, int t) {
+    
+int k = 0;
+        for(int i=n;i<=100;i++){
+            int c = i;
+            int product = 1;
+
+            while(c >0){
+                product *= c%10;
+                c = c/10;
+            }
+            if(product % t == 0){
+                k = i;
+                break;
+            }
+        }
+        return k;
+    }
+
