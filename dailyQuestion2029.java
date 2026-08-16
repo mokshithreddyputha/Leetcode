010c26fa0358dfd9ef17f48bc5ec86af4e1f3db1
Mokshith Reddy <mokshithreddyputha8@gmail.com>
2026-08-16 05:54:08 -0500

    Stone game - 6 in java

---
+++ dailyQuestion2029.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int[] count = new int[3];
+
+        // Count remainders
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        // Even number of 0-modulo stones
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        // Odd number of 0-modulo stones
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
